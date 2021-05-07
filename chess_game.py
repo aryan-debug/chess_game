a7269478f4729d9b59b4fff4f7c0582eb429b132
Aryan Arora <arynarora232004@gmail.com>
2021-05-07 13:43:13 -0700

    pawns can move

---
+++ chess_game.py
@@ -0,0 +1,160 @@
+import pygame
+import sys
+
+pygame.init()
+
+RANKS = 8
+FILES = 8
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+RECT_WIDTH = 100
+RECT_HEIGHT = 100
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+green_color = (118,150,86)
+white_color = (238,238,210)
+move_color_dict = {'white':'black','black':'white'}
+
+class Board:
+    def __init__(self,ranks, files, rect_width, rect_height):
+        self.ranks = ranks
+        self.files = files
+        self.rect_width = rect_width
+        self.rect_height = rect_height
+        self.rect_pos_x = 0
+        self.rect_pos_y = 0
+        self.color_counter = 0
+        self.actual_board = self.make_board(self.ranks, self.files)
+        self.move_color = 'white'
+
+    def draw_board(self):
+        for rank in range(self.ranks):
+            for file in range(self.files):
+                if self.rect_pos_x >= SCREEN_WIDTH:
+                    self.rect_pos_x = 0
+                    self.rect_pos_y += self.rect_height
+                if self.color_counter % 2 == 0:
+                    current_color = white_color
+                pygame.draw.rect(screen,current_color, pygame.Rect(self.rect_pos_x,self.rect_pos_y,self.rect_width,self.rect_width))
+                self.rect_pos_x += self.rect_width
+                current_color = green_color
+                self.color_counter += 1
+            self.color_counter += 1
+        self.rect_pos_x = 0
+        self.rect_pos_y = 0
+        self.color_counter = 0
+        pygame.display.flip()
+
+    def make_board(self,ranks,files):
+        _2d_board = [[Rook("black"),Knight("black"),Bishop("black"),Queen("black"),King("black"),Bishop("black"),Knight("black"),Rook("black")],
+        [Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black")]]
+
+        for rank in range(2,self.ranks-2):
+            _2d_board.append([])
+            for file in range(self.files):
+                _2d_board[rank].append(None)
+
+        _2d_board.append([Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white")])
+        _2d_board.append([Rook("white"),Knight("white"),Bishop("white"),Queen("white"),King("white"),Bishop("white"),Knight("white"),Rook("white")])
+        return _2d_board
+    
+    def draw_pieces(self, board):
+        for x, row in enumerate(board):
+            for y, position in enumerate(row):
+                if position:
+                    position.draw(y*100,x*100)
+class Piece:
+    def __init__(self, color, piece_type):
+        self.color = color
+        self.piece_type = piece_type
+        self.image = pygame.image.load(f'{self.color}_{self.piece_type}.png')
+
+    def draw(self, x, y):
+        screen.blit(self.image, (x, y))
+
+class Pawn(Piece):
+    def __init__(self,color):
+        self.piece_type = "pawn"
+        self.first_move = True
+        super().__init__(color,self.piece_type)
+
+    def generated_valid_move(self, previous_y, previous_x):
+        offset = {"black":1,"white":-1}
+        valid_moves_list = []
+        #check for first move
+        if self.first_move:
+            valid_moves_list.append((previous_y + offset[self.color] * 2, previous_x))
+            self.first_move = False
+        #check for piece on front
+        if actual_board[previous_y + offset[self.color]][previous_x] == None:
+            valid_moves_list.append((previous_y + offset[self.color],previous_x))
+        #check for piece on right side
+        try:
+            if actual_board[previous_y + offset[self.color]][previous_x+1]:
+                if actual_board[previous_y + offset[self.color]][previous_x+1].color != self.color:
+                        valid_moves_list.append((previous_y + offset[self.color],previous_x+1))
+        except IndexError:
+            pass
+        #check for piece on left side
+        if actual_board[previous_y + offset[self.color]][previous_x-1]:
+            if actual_board[previous_y + offset[self.color]][previous_x-1].color != self.color:
+                    valid_moves_list.append((previous_y+offset[self.color],previous_x-1))
+        print(valid_moves_list)
+        return valid_moves_list
+    
+class Knight(Piece):
+    def __init__(self, color):
+        self.piece_type = 'knight'
+        super().__init__(color, self.piece_type)
+    def generated_valid_move(self, previous_y, previous_x):
+        pass
+
+class Bishop(Piece):
+    def __init__(self, color):
+        self.piece_type = 'bishop'
+        super().__init__(color, self.piece_type)
+        
+class Rook(Piece):
+    def __init__(self,color):
+        self.piece_type = 'rook'
+        super().__init__(color, self.piece_type)
+
+class Queen(Piece):
+    def  __init__(self, color):
+        self.piece_type = "queen"
+        super().__init__(color,self.piece_type)
+
+class King(Piece):
+    def __init__(self, color):
+        self.piece_type = 'king'
+        super().__init__(color, self.piece_type)
+
+board = Board(RANKS,FILES, RECT_WIDTH, RECT_HEIGHT)
+board.draw_board()
+actual_board = board.make_board(RANKS,FILES)
+board.draw_pieces(actual_board)
+pygame.display.update()
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.display.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            x,y = event.pos
+            previous_x, previous_y = x//100,y//100
+            current_piece = actual_board[previous_y][previous_x]
+        
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            if current_piece != None and board.move_color == current_piece.color:
+                x,y = event.pos
+                new_x, new_y = x//100,y//100
+                print(new_y,new_x)
+                if (new_y,new_x) in current_piece.generated_valid_move(previous_y, previous_x):
+                    actual_board[new_y][new_x] = current_piece
+                    actual_board[previous_y][previous_x] = None
+                    board.draw_board()
+                    board.draw_pieces(actual_board)
+                    board.move_color = move_color_dict[current_piece.color]
+                    pygame.display.update()
+
+            
+
